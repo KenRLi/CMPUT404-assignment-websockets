fccb4293cbebc8103267671d5592bf3df15e23ee
Ken Li <krli@ualberta.ca>
2020-03-20 17:22:30 -0600

    It runs, doesn't work

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -26,63 +26,110 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+# - From chat.py Example -
+# https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/chat.py
+clients = list()
+
+def send_all(msg):
+    for client in clients:
+        client.put(msg)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+# ---
+
 class World:
     def __init__(self):
         self.clear()
-        # we've got listeners now!
         self.listeners = list()
-        
-    def add_set_listener(self, listener):
-        self.listeners.append( listener )
+
+    def set(self, entity, data):
+        self.space[entity] = data
+        self.update_listeners(entity)
 
     def update(self, entity, key, value):
         entry = self.space.get(entity,dict())
         entry[key] = value
         self.space[entity] = entry
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
-    def set(self, entity, data):
-        self.space[entity] = data
-        self.update_listeners( entity )
+    # Listener Functions
+    def add_set_listener(self, listener):
+        self.listeners.append(listener)
 
     def update_listeners(self, entity):
-        '''update the set listeners'''
         for listener in self.listeners:
             listener(entity, self.get(entity))
 
-    def clear(self):
-        self.space = dict()
-
+    # World Functions
     def get(self, entity):
         return self.space.get(entity,dict())
     
     def world(self):
         return self.space
 
+    def clear(self):
+        self.space = dict()
+
+
 myWorld = World()        
 
-def set_listener( entity, data ):
-    ''' do something with the update ! '''
-
-myWorld.add_set_listener( set_listener )
+def set_listener(entity, data):
+    # do something with the update
+    message = json.dumps({entity:data})
+    for client in clients:
+        client.put(message)
+    
+myWorld.add_set_listener(set_listener)
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect(url_for('static', filename='index.html'), 301)
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # From chat.py Example
+    try:
+        while True:
+            msg = ws.receive()
+            if (msg is not None):
+                packet = json.loads(msg)
+                send_all_json( packet )
+            else:
+                break
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
-
+    # From chat.py Example
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)    
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -99,25 +146,29 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json
+    if (request.method == 'POST'):
+        for key, value in data.items():
+            myWorld.update(entity, key, value)
+    else:
+        myWorld.set(entity, data)
+    return json.dumps(myWorld.get(entity)), 200
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.world()), 200
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    return json.dumps(myWorld.get(entity)), 200
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    return redirect(url_for('static', filename='index.html'), 301)
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
